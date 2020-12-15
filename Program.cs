43cabc315784759e96d3bf85cfa0cb62a7346401
unknown <abossard@gmail.com>
2020-12-15 23:25:06 +0100

    that's how I would do it

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 namespace ConsoleApp1
 {
-    class Result
+    class ResultUselessHelpersIgnore
     {
         public static Result<TD, Exception> Ok<TD>(TD data)
         {
@@ -17,7 +17,7 @@ namespace ConsoleApp1
 
     
     
-    class Result<TD, TE> : Result where TE: Exception
+    public class Result<TD, TE>
     {
         public Result(TD data, TE error)
         {
@@ -35,28 +35,38 @@ namespace ConsoleApp1
 
     class Program
     {
-        static Result<string, Exception> TryToCreateResult()
-        {
-            var rand = new Random();
-            return rand.NextDouble() >= 0.5 ? Result.Ok("Hi") : Result.FailureString(new Exception("Big freaking problem"));
-        }
-        
+       
         static Result<string, Exception> TryToCreateResultWithoutHelper()
         {
             var rand = new Random();
             return rand.NextDouble() >= 0.5 ? new("hi", null) : new(null, new Exception("Big freaking problem"));
         }
+        
+        static Result<bool, string> TryToCreateResultWithoutHelper2()
+        {
+            var rand = new Random();
+            return rand.NextDouble() >= 0.5 ? new(true, null) : new(false, "Big freaking problem");
+        }
 
         static void Main(string[] args)
         {
             var result = TryToCreateResultWithoutHelper();
+            
             var message = result switch
             {
                 {IsSuccess: true, Data: var data} => $"Got some: {data}",
                 {IsSuccess: false, Error: var errorData} => $"Oops {errorData}",
             };
 
+            var result2 = TryToCreateResultWithoutHelper2();
+            
+            var message2 = result2 switch
+            {
+                {IsSuccess: true, Data: var data} => $"Got some: {data}",
+                {IsSuccess: false, Error: var errorData} => $"Oops {errorData}",
+            };
             Console.WriteLine(message);
+            Console.WriteLine(message2);
         }
     }
 }
